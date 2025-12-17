a5d36bb126da2e76036f334f3db7cd4120a19fb2
Drullkus <5010174+Drullkus@users.noreply.github.com>
2025-12-16 22:03:43 -0800

    PackSource.FEATURE is slightly different than expected...

--- src/main/java/teamtwilight/berryovergrown/BerryOvergrown.java
+++ src/main/java/teamtwilight/berryovergrown/BerryOvergrown.java
@@ -1,5 +1,6 @@
 package teamtwilight.berryovergrown;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.PackType;
@@ -21,6 +22,18 @@ public class BerryOvergrown {
     public static final String MODID = "berry_overgrown";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+	private final PackSource builtInNotAuto = new PackSource() {
+		@Override
+		public Component decorate(Component title) {
+			return Component.translatable("pack.nameAndSource", title, Component.translatable("pack.source.builtin")).withStyle(ChatFormatting.GRAY);
+		}
+
+		@Override
+		public boolean shouldAddAutomatically() {
+			return false;
+		}
+	};
+
 	public BerryOvergrown(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::registerDataPacks);
 
@@ -30,11 +43,22 @@ public class BerryOvergrown {
 	private void registerDataPacks(AddPackFindersEvent event) {
 		if (event.getPackType() != PackType.SERVER_DATA) return;
 
-		LOGGER.debug("Adding Berry Overgrown packs.");
+		this.addPack(event, "ore", "Twilight Forest: Ore Berries", Config.ORE_BERRIES.getAsBoolean());
+		this.addPack(event, "nether", "Twilight Forest: Nether Berries", Config.NETHER_BERRIES.getAsBoolean());
+		this.addPack(event, "overworld", "Twilight Forest: Overworld Berries", Config.OVERWORLD_BERRIES.getAsBoolean());
+	}
 
-		event.addPackFinders(modid("datapacks/ore"), PackType.SERVER_DATA, Component.literal("Twilight Forest: Ore Berries"), PackSource.BUILT_IN, Config.ORE_BERRIES.getAsBoolean(), Pack.Position.TOP);
-		event.addPackFinders(modid("datapacks/nether"), PackType.SERVER_DATA, Component.literal("Twilight Forest: Nether Berries"), PackSource.BUILT_IN, Config.NETHER_BERRIES.getAsBoolean(), Pack.Position.TOP);
-		event.addPackFinders(modid("datapacks/overworld"), PackType.SERVER_DATA, Component.literal("Twilight Forest: Overworld Berries"), PackSource.BUILT_IN, Config.OVERWORLD_BERRIES.getAsBoolean(), Pack.Position.TOP);
+	private void addPack(AddPackFindersEvent event, String packFolder, String name, boolean autoAdd) {
+		LOGGER.debug(autoAdd ? "Adding {} pack to activated column" : "Adding {} pack to deactivated column", name);
+
+		event.addPackFinders(
+				modid("datapacks/" + packFolder),
+				PackType.SERVER_DATA,
+				Component.literal(name),
+				autoAdd ? PackSource.BUILT_IN : this.builtInNotAuto,
+				autoAdd,
+				Pack.Position.TOP
+		);
 	}
 
 	public static ResourceLocation modid(String path) {
